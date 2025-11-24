d7db4e19a6e7dab27c37d073d71cbe1499d0d2ad
dav3794 <dav3794@protonmail.com>
2025-11-24 18:40:53 +0100

    Add loading data from .npy to dataset

--- multiplex_model/data.py
+++ multiplex_model/data.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, Sampler
 from torchvision.transforms.functional import rotate, crop
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Literal
 from glob import glob
 import tifffile
 from cv2 import medianBlur
@@ -18,26 +18,31 @@ class DatasetFromTIFF(Dataset):
             split: str,
             marker_tokenizer: Dict[str, int],
             transform=None,
+            use_preprocessing: bool = True,
             use_median_denoising: bool = False,
             use_butterworth_filter: bool = True,
             use_minmax_normalization: bool = False,
             use_clip_normalization: bool = True,
             global_upper_bound: float = 5.0,
             use_global_clip_limits: bool = False,
+            file_extension: Literal['tiff', 'npy'] = 'tiff',
         ):
-        """Dataset for loading TIFF images from multiple panels.
+        """Dataset for loading multiplex images from multiple panels.
 
         Args:
             panels_config (Dict): Configuration dictionary for panels.
             split (str): Name of the data split (e.g., 'train', 'val', 'test').
             marker_tokenizer (Dict[str, int]): Tokenizer for marker names.
             transform (_type_, optional): Transform to be applied to the images. Defaults to None.
+            use_preprocessing (bool, optional): Whether to use preprocessing. Defaults to True.
             use_median_denoising (bool, optional): Whether to use median denoising. Defaults to False.
             use_butterworth_filter (bool, optional): Whether to use Butterworth filter. Defaults to True.
             use_minmax_normalization (bool, optional): Whether to use min-max normalization. Defaults to True.
             use_clip_normalization (bool, optional): Whether to use clipping normalization. Defaults to False.
             global_upper_bound (float, optional): Global upper bound for clipping normalization if `clip_limits` 
                 is not provided in config. Defaults to 5.0.
+            use_global_clip_limits (bool, optional): Whether to use global clip limits for all datasets. Defaults to False.
+            file_extension (Literal['tiff', 'npy'], optional): File extension of the images. Defaults to 'tiff'.
         """
         assert 'paths' in panels_config, "Panels config must have 'paths' attribute with paths of splits of the data."
         assert split in panels_config['paths'], f"Panels config must have '{split}' attribute with data path."
@@ -55,7 +60,7 @@ class DatasetFromTIFF(Dataset):
         img_path = panels_config['paths'][split]
         self.imgs = [] # tuples of (img_path, dataset)
         for dataset in panels_config['datasets']:
-            tiffs = glob(os.path.join(img_path, dataset, 'imgs', '*.tiff'))
+            tiffs = glob(os.path.join(img_path, dataset, 'imgs', f'*.{file_extension}'))
             self.imgs.extend([(tiff, dataset) for tiff in tiffs])
 
         if use_global_clip_limits:
@@ -69,6 +74,13 @@ class DatasetFromTIFF(Dataset):
         self.use_butterworth = use_butterworth_filter
         self.use_minmax_normalization = use_minmax_normalization
         self.use_clip_normalization = use_clip_normalization
+        self.use_preprocessing = use_preprocessing
+        self.file_extension = file_extension
+        self.read_file_func = (
+            tifffile.imread
+            if self.file_extension == 'tiff' 
+            else np.load
+        )
 
     @staticmethod
     def preprocess(img):
@@ -100,13 +112,13 @@ class DatasetFromTIFF(Dataset):
             (img - min_val) / (max_val - min_val + 1e-8)
         )
         scaled_img = np.clip(scaled_img, 0, 1)
-        return torch.tensor(scaled_img)
+        return scaled_img
     
     def norm_clip(self, img, dataset):
         """Normalize image channels to [0, 1] range using clipping."""
         upper_bound = self.clip_limits.get(dataset, self.global_upper_bound)
         img = np.clip(img, 0, upper_bound) / upper_bound
-        return torch.tensor(img)
+        return img
 
     def __len__(self):
         return len(self.imgs)
@@ -115,8 +127,9 @@ class DatasetFromTIFF(Dataset):
         img_path, dataset = self.imgs[idx]
         channel_ids = self.channel_ids[dataset]
 
-        img = tifffile.imread(img_path)
-        img = self.preprocess(img)
+        img = self.read_file_func(img_path)
+        if self.use_preprocessing:
+            img = self.preprocess(img)
 
         if self.transform:
             img = self.transform(torch.tensor(img)).numpy()
@@ -133,7 +146,9 @@ class DatasetFromTIFF(Dataset):
         elif self.use_minmax_normalization:
             img = self.norm_minmax(img)
         
-        return torch.tensor(img), channel_ids, dataset, img_path
+        img = torch.tensor(img)
+        
+        return img, channel_ids, dataset, img_path
 
 
 class SegmentationDataset(Dataset):
